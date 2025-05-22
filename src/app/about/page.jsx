cbc97d082840c68beeda210f70dea6ad8325a694
Mmsshakil <softhandshakil@gmail.com>
2025-05-23 03:18:39 +0600

    navigate by useRouter

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,29 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const About = () => {
+  const router = useRouter();
+  const loggedIn = false;
+  const handleNavigation = () => {
+    if (loggedIn) {
+      router.push("/about/address");
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
     <div>
       <p>About</p>
+      <Link href="/about/address">
+        <p className="font-bold text-center my-3 text-amber-600">Addres Page</p>
+      </Link>
+
+      <button type="button" onClick={handleNavigation}>
+        Address Page
+      </button>
     </div>
   );
 };
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <nav>
       <ul className="flex justify-center gap-10">
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/services">
+          <li>Services</li>
+        </Link>
       </ul>
     </nav>
   );
